b699f834c8d926480c493125dc72a213650b3216
alekslog <htchtc052@gmail.com>
2023-11-17 00:29:11 +0300

    Email confirmation flow and auth refactoring

---
+++ backend/src/tokens/types/JwtEmailVerificationPayload.ts
@@ -0,0 +1,3 @@
+export type JwtEmailConfirmationPayload = {
+  sub: string;
+};
--- backend/src/users/types/user.types.ts
+++
@@ -1,5 +0,0 @@
-import { Prisma, User } from '@prisma/client';
-
-export type UserUpdateInput = Partial<User>;
-
-export type UserUncheckedCreateInput = Prisma.UserUncheckedCreateInput;
